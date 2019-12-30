277dea904683ad87136b1d4e1271836b6f9f7ccf
Aux <justin@akitaux.com>
2019-12-29 18:40:08 -0600

    initial

---
+++ src/Terrier/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Terrier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
